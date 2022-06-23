6f9747f45c32b20fa8c2e56ad898416df472e3af
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-23 14:14:58 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class multiply{
+      public static void main(String[]args){
+  
+       Scanner SC = new Scanner(System.in);
+       System.out.println(" input a no ");
+        int a = SC.nextInt();
+
+       System.out.println(" input another no");
+       int b = SC.nextInt();
+       
+      int multi = a * b;
+       System.out.println(multi);
+}
+
+}
+       
+       
+       
