f0e8c2229254edf99baccc0cfeee731df69d211b
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-23 14:15:21 +0530

    Add files via upload

---
+++ float1.java
@@ -0,0 +1,16 @@
+
+class float1{
+    public static void main(String[] args)
+    {
+  
+        // f is to ensures that numbers are float DATA TYPE
+        float f1 = 2.5f;
+        float f2 = 4.6f;
+  
+        // to store the multiplied value
+        float m = f1 * f2;
+  
+        // to print the product
+        System.out.println(m);
+    }
+}
