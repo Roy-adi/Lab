58ace8ff308422e97dc551a9f8cc4a03d4fd9861
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-23 14:14:01 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ class user{
+  public static void main(String []args){
+   
+   Scanner SC = new Scanner(System.in);
+   System.out.println(" input your line: " );
+   String line = SC.nextLine();
+   System.out.println(line);
+}
+
+
+}   
