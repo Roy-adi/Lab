213dfff8a3022d5ad5c540a1fbaa3ab4865c3567
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-24 11:04:18 +0530

    Update user.java

--- user.java
+++ user.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
  class user{
   public static void main(String []args){
    
-   Scanner SC = new Scanner(System.in);
-   System.out.println(" input your line: " );
-   String line = SC.nextLine();
-   System.out.println(line);
+   Scanner SC = new Scanner(System.in);       // input from user
+   System.out.println(" input your line: " );  // input the line
+   String line = SC.nextLine();                // the line is in string
+   System.out.println(line);                   // print the line as output
 }
 
 
