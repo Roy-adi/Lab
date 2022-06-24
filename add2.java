5b39ebe069dd82c1b15e038ba133499f82c17fe0
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-24 10:57:14 +0530

    Update add2.java

--- add2.java
+++ add2.java
@@ -1,10 +1,10 @@
-class add2{
+class add2{          // take the file name as class name
      public static void main(String[]args){
 
-     int a= 4;
-     int b=6;
-     int add = a+b;
-     System.out.println(add);
+     int a= 4;         // take the 1st int
+     int b=6;          // take the 2nd int
+     int add = a+b;    // take the a int to add those value & store the value there
+     System.out.println(add);    // print the value
 }
 
 }
