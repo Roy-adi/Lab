877a3beedf6646205c75a45c6ddd6f61e9e0735f
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-24 11:02:19 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 class multiply{
       public static void main(String[]args){
   
-       Scanner SC = new Scanner(System.in);
+       Scanner SC = new Scanner(System.in);  // To input from user 
        System.out.println(" input a no ");
-        int a = SC.nextInt();
+        int a = SC.nextInt();               // input in form of int
 
        System.out.println(" input another no");
        int b = SC.nextInt();
        
-      int multi = a * b;
-       System.out.println(multi);
+      int multi = a * b;                  // multiply those no & store in a int
+       System.out.println(multi);         // print the product
 }
 
 }
