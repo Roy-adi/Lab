392a9d72b83bd108ec613fdeef54ffc40700328b
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-30 15:34:43 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class prime {
+
+   public static void main(String[] args)
+   {
+      int num ;
+      int i;
+      int count=0;
+      Scanner SC = new Scanner(System.in);
+      
+      System.out.print("Enter a Number: ");
+      num = SC.nextInt();
+      
+      for(i=2; i<num; i++)
+      {
+         if(num%i == 0)
+         {
+            count++;
+            break;
+         }
+      }
+      
+      if(count==0)
+         System.out.println("\nIt is a Prime Number.");
+      else
+         System.out.println("\nIt is not a Prime Number.");
+   }
+}
