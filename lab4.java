1877c263c7e307485a79d91d67a1272d7f01427d
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-07-29 10:16:00 +0530

    Create lab4.java

---
+++ lab4.java
@@ -0,0 +1,10 @@
+package default1;
+public class lab4 {
+    public static void main(StrictMath[]argsMaths){
+        String st = "Welcome to java World";
+        System.out.println("Character at index 5th "+ st.charAt(5));
+        System.out.println("Comparing;"+"welcome".compareToIgnoreCase(st));
+        System.out.println("concatenting"+ st +"let us learn ");
+        System.out.println("lowercasing "+ st.toLowerCase());
+    }
+}
